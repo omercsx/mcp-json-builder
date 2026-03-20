6816b5390745b18215d921e036c1e07eb4260674
omercsx <omercagris@gmail.com>
2026-03-20 16:26:18 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>MCP JSON Builder</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
